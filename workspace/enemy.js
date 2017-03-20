53322c30d08ebbebd70b337af66501c879045fea
Alex <ahansen2@trinity.edu>
2017-03-20 13:24:06 -0500

    enemies spawn in random places

--- workspace/enemy.js
+++ workspace/enemy.js
@@ -19,6 +19,7 @@ class Enemy {
 		ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
 		ctx.fill();
 	}
+
 	// Removes health from the unit
 	damage(x) {
 		this.health = this.health - x;
@@ -26,27 +27,33 @@ class Enemy {
 			this.alive = false;
 		}
 	}
+
+	// Determines if the enemy has arrived at your base.
 	arrived() {
-		if (this.y > 600) {
+		if (this.y > canvas.height - this.radius) {
 			return true;
 		}
 		else {
 			return false;
 		}
 	}
+
+	// Moves the enemy's y pos by its velocity.
 	move() {
 		this.y = this.y + this.v;
 	}
 
 }
+
 function createEnemy(type) {
 	var enemy;
 	switch (type) {
 		// TODO determine algorithm for enemy type choice
-		// TODO determine where they spawn
+		// TODO determine where they spawn - right now it is just random
 		// TODO decide on enemy types
 		case 1:
-			enemy = new Enemy(2, 10, 10, 2, 'red', 100, 25, 20);
+			var x = Math.floor(Math.random() * 600);
+			enemy = new Enemy(2, x, 10, 2, 'red', 100, 25, 20);
 			break;
 		default:
 			console.log("invalid enemy type");
