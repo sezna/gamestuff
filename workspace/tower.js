91ef2ee5007da89db00b3f4d85427f5032deb488
Alex <ahansen2@trinity.edu>
2017-03-20 11:35:56 -0500

    not sure what is different

--- workspace/tower.js
+++ workspace/tower.js
@@ -30,10 +30,15 @@ function buildTower(type) {
 	var tower;
 	switch (type) {
 		// TODO actually pick a location
+		// this is for you mary, make this function take in a location
+		// that is from a mouse click
+		// TODO decide on tower types
 		case 1:
 			tower = new Tower(1, 0, 0, 'red', 'red', 100, 10, 10);
 			break;
-			
+		case 2:
+			tower = new Tower(1, 0, 0, 'blue', 'blue', 100, 10, 10);
+			break;
 		default:
 			console.log("invalid tower type");
 	}
