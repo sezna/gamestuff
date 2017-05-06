80b70a89f829f074e47b23bb21dcbeefa49175cd
mmaron0 <mmaron@trinity.edu>
2017-05-06 16:56:31 -0500

    Got rid of jquery

--- play-with-scalajs-example-master/client/src/main/scala/example/ScalaJSExample.scala
+++ play-with-scalajs-example-master/client/src/main/scala/example/ScalaJSExample.scala
@@ -9,14 +9,24 @@ import scala.scalajs.js.annotation.JSExportTopLevel
 
 
 object ScalaJSExample extends js.JSApp {
-  def pollList(): Unit = {
-    println("In polling")
-   
-  }
-
-  def main(): Unit = {
-//    dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
+   def main(): Unit = {
     println("Set up polling")
     scalajs.js.timers.setInterval(500)(pollList())
+    /*jQuery("#button").click((event: Any) => {
+      drawRandom()
+    })*/
+  }
+  
+  def drawRandom(): Unit = {
+    //jQuery("#random").load("/random")
+  }
+
+  def pollList(): Unit = {
+    //jQuery("#students").load("/list")
+  }
+
+  @JSExportTopLevel("shoutOut")
+  def shoutOut(): Unit = {
+    dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
   }
 }
